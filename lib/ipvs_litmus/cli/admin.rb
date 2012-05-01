323b6eba12c3886f7941a7bd45c7ac41365d130a
ben and tony <code@getbraintree.com>
2012-05-01 15:43:56 -0500

    litmusctl down command and delete option for up/down.

--- lib/ipvs_litmus/cli/admin.rb
+++ lib/ipvs_litmus/cli/admin.rb
@@ -6,10 +6,53 @@ module IPVSLitmus
         send(command, argv)
       end
 
+      def down(args)
+        options = { :port => 9292, :host => 'localhost' }
+        opt_parser = OptionParser.new do |opts|
+          opts.banner = "Usage: litmusctl down <service> [options]"
+
+          opts.on("-d", "--delete", "Remove downfile") do
+            options[:delete] = true
+          end
+
+          opts.on("-r", "--reason=reason", String, "Reason for downfile") do |reason|
+            options[:reason] = reason
+          end
+
+          opts.on("-p", "--port=port", Integer, "Port litmus is running on", "Default: 9292") do |port|
+            options[:port] = port
+          end
+          opts.on("-h", "--host=ip", String, ":Host litmus is running on", "Default: localhost") do |host|
+            options[:host] = host
+          end
+          opts.on("--help", "Show this help message.") { puts opts; exit }
+        end
+
+        opt_parser.parse! args
+        service = args.shift
+
+        if options[:delete]
+          request = Net::HTTP::Delete.new("/force/down/#{service}")
+        else
+          if !options.has_key?(:reason)
+            print "Reason? "
+            options[:reason] = gets.chomp
+          end
+          request = Net::HTTP::Post.new("/force/down/#{service}")
+          request.set_form_data('reason' => options[:reason])
+        end
+
+        _litmus_request(options[:host], options[:port], request)
+      end
+
       def up(args)
         options = { :port => 9292, :host => 'localhost' }
         opt_parser = OptionParser.new do |opts|
           opts.banner = "Usage: litmusctl up <service> [options]"
+
+          opts.on("-d", "--delete", "Remove downfile") do
+            options[:delete] = true
+          end
 
           opts.on("-r", "--reason=reason", String, "Reason for upfile") do |reason|
             options[:reason] = reason
@@ -27,23 +70,18 @@ module IPVSLitmus
         opt_parser.parse! args
         service = args.shift
 
-        if !options.has_key?(:reason)
-          print "Reason? "
-          options[:reason] = gets.chomp
+        if options[:delete]
+          request = Net::HTTP::Delete.new("/force/up/#{service}")
+        else
+          if !options.has_key?(:reason)
+            print "Reason? "
+            options[:reason] = gets.chomp
+          end
+          request = Net::HTTP::Post.new("/force/up/#{service}")
+          request.set_form_data('reason' => options[:reason])
         end
 
-        begin
-          response = Net::HTTP.post_form(URI.parse("http://#{options[:host]}:#{options[:port]}/force/up/#{service}"), :reason => options[:reason])
-          puts response.body
-          case response
-          when Net::HTTPSuccess then exit 0
-          when Net::HTTPClientError then exit 2
-          else exit 1
-          end
-        rescue Errno::ECONNREFUSED => e
-          puts "Unable to connect to litmus on #{options[:host]}:#{options[:port]}: #{e.message}"
-          exit 1
-        end
+        _litmus_request(options[:host], options[:port], request)
       end
 
       def status(args)
@@ -62,8 +100,15 @@ module IPVSLitmus
         opt_parser.parse! args
         service = args.shift
 
+        _litmus_request(options[:host], options[:port], Net::HTTP::Get.new("/#{service}/status"))
+      end
+
+
+      def _litmus_request(host, port, request)
         begin
-          response = Net::HTTP.get_response(URI.parse("http://#{options[:host]}:#{options[:port]}/#{service}/status"))
+          http = Net::HTTP.start(host, port)
+          response = http.request(request)
+
           puts response.body
           case response
           when Net::HTTPSuccess then exit 0
@@ -71,7 +116,7 @@ module IPVSLitmus
           else exit 1
           end
         rescue Errno::ECONNREFUSED => e
-          puts "Unable to connect to litmus on #{options[:host]}:#{options[:port]}: #{e.message}"
+          puts "Unable to connect to litmus on #{host}:#{port}: #{e.message}"
           exit 1
         end
       end
