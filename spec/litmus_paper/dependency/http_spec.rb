0127bb4d281fd39134a4e2b64bec8a004d485d93
tony <code@getbraintree.com>
2012-06-04 16:29:39 -0500

    removes puts

--- spec/litmus_paper/dependency/http_spec.rb
+++ spec/litmus_paper/dependency/http_spec.rb
@@ -25,8 +25,7 @@ describe LitmusPaper::Dependency::HTTP do
     context "https" do
       it "can make https request" do
         begin
-          LitmusPaper.logger = StdoutLogger.new
-          puts `env SSL_TEST_PORT=9295 PID_FILE=/tmp/https-test-server.pid bundle exec spec/script/https_test_server.rb`
+          `env SSL_TEST_PORT=9295 PID_FILE=/tmp/https-test-server.pid bundle exec spec/script/https_test_server.rb`
           SpecHelper.wait_for_service :host => '127.0.0.1', :port => 9295
 
           check = LitmusPaper::Dependency::HTTP.new(
