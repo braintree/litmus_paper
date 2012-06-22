f198eba727e7bdb0177e8f5e5c6b4e4e88de75e1
dan and tony <code@getbraintree.com>
2012-06-22 10:14:32 -0500

    logging to get more info about why this test is broken

--- spec/litmus_paper/dependency/haproxy_backends_spec.rb
+++ spec/litmus_paper/dependency/haproxy_backends_spec.rb
@@ -5,8 +5,9 @@ describe LitmusPaper::Dependency::HaproxyBackends do
     before(:each) do
       @file = File.expand_path("stub-haproxy-stats")
       FileUtils.rm_rf(@file)
-      system "spec/support/haproxy_test_socket #{@file} &"
+      system "spec/support/haproxy_test_socket #{@file} > log &"
       sleep 1
+      puts File.read("log")
     end
 
     it "is available if at least one backend is up" do
