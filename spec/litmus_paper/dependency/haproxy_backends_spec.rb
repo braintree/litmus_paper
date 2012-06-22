9e3483a2b5204db1fdff55d481d6983d74e6001b
dan and tony <code@getbraintree.com>
2012-06-22 10:09:55 -0500

    test not putting domain socket in tmp

--- spec/litmus_paper/dependency/haproxy_backends_spec.rb
+++ spec/litmus_paper/dependency/haproxy_backends_spec.rb
@@ -3,18 +3,19 @@ require 'spec_helper'
 describe LitmusPaper::Dependency::HaproxyBackends do
   describe "available?" do
     before(:each) do
-      FileUtils.rm_rf("/tmp/stub-haproxy-stats")
-      system "spec/support/haproxy_test_socket /tmp/stub-haproxy-stats &"
+      @file = File.expand_path("stub-haproxy-stats")
+      FileUtils.rm_rf(@file)
+      system "spec/support/haproxy_test_socket #{@file} &"
       sleep 1
     end
 
     it "is available if at least one backend is up" do
-      haproxy = LitmusPaper::Dependency::HaproxyBackends.new("/tmp/stub-haproxy-stats", "yellow_cluster")
+      haproxy = LitmusPaper::Dependency::HaproxyBackends.new(@file, "yellow_cluster")
       haproxy.should be_available
     end
 
     it "returns 0 if no nodes are available" do
-      haproxy = LitmusPaper::Dependency::HaproxyBackends.new("/tmp/stub-haproxy-stats", "orange_cluster")
+      haproxy = LitmusPaper::Dependency::HaproxyBackends.new(@file, "orange_cluster")
       haproxy.should_not be_available
     end
   end
