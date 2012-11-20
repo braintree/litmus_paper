b48ace008189cc0e0ac009b680bdd39ee695dd51
tony <code@getbraintree.com>
2012-11-20 10:24:26 -0600

    adds logging around the script dependency

--- lib/litmus_paper/dependency/script.rb
+++ lib/litmus_paper/dependency/script.rb
@@ -8,10 +8,15 @@ module LitmusPaper
 
       def available?
         Timeout.timeout(@timeout) do
-          %x[#{@command}]
+          output = %x[#{@command}]
+          unless $CHILD_STATUS.success?
+            LitmusPaper.logger.info("Available check to #{@command} failed with status #{$CHILD_STATUS.exitstatus}")
+            LitmusPaper.logger.info("Failed output #{output}")
+          end
           $CHILD_STATUS.success?
         end
       rescue Timeout::Error
+        LitmusPaper.logger.info("Available check to '#{@command}' timed out")
         false
       end
 
